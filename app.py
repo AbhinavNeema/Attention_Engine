dda9975d7a1d61b16e8ff134887a57dd7c3d830c
AbhinavNeema <abhinavneema22gmail.com>
2026-01-20 19:54:35 +0530

    changed app.py for deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import numpy as np
 import cv2
+import os
 from attention import analyze_frame
 
 app = Flask(__name__)
@@ -39,4 +40,5 @@ def analyze():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7001)
+    port = int(os.environ.get("PORT", 7001))
+    app.run(host="0.0.0.0", port=port)
